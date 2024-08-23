1da6d923f6c5fdc9564074254a7ffdb4137104f2
saile <eliasiv.codes@gmail.com>
2024-08-23 00:06:49 -0700

    implement addSong resolver

--- src/resolvers/song/SongResolver.ts
+++ src/resolvers/song/SongResolver.ts
@@ -1,11 +1,12 @@
 import { Arg, Mutation, Resolver } from "type-graphql";
 import songRepo from "./songRepo";
 import AddSongInput from "./inputs/AddSongInput";
+import Song from "../../entities/Song";
 
 @Resolver()
 export default class SongResolver {
-  @Mutation(() => Boolean)
+  @Mutation(() => Song)
   async addSong(@Arg("addSongInput") addSongInput: AddSongInput) {
-    return await songRepo.addSong(addSongInput);
+    return (await songRepo.addSong(addSongInput))!;
   }
 }
--- src/resolvers/song/songRepo.ts
+++ src/resolvers/song/songRepo.ts
@@ -1,12 +1,18 @@
 import dataSource from "../../dataSource";
 import Song from "../../entities/Song";
+import artistRepo from "../artist/artistRepo";
 import AddSongInput from "./inputs/AddSongInput";
 
 const songRepo = dataSource.getRepository(Song).extend({
-  async addSong(addSongInput: AddSongInput) {
-    const newSong = this.create(addSongInput);
+  async addSong({ artistId, ...rest }: AddSongInput) {
+    const songArtist = await artistRepo.findOneBy({ id: artistId });
 
-    return (await this.insert(newSong)).identifiers.length > 0;
+    if (!songArtist) return null;
+
+    const newSong = this.create({ artist: songArtist, ...rest });
+    const newSongId = (await this.insert(newSong)).identifiers[0].id;
+
+    return await this.findOne({ where: { id: newSongId }, relations: { artist: true } });
   },
 });
 
